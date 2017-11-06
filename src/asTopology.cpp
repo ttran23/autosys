2e1e6f918168358dd4a975369ac3f1418feedf04
Jason <ichikasuto@gmail.com>
2017-11-05 17:42:03 -0700

    Created topology files

---
+++ src/asTopology.cpp
@@ -0,0 +1,8 @@
+//
+//  asTopology.cpp
+//  autosys
+//
+//  Created by Jason Tran on 11/5/17.
+//
+
+#include "asTopology.h"
---
+++ src/asTopology.h
@@ -0,0 +1,13 @@
+//
+//  asTopology.h
+//  autosys
+//
+//  Created by Jason Tran on 11/5/17.
+//
+
+#ifndef asTopology_h
+#define asTopology_h
+
+#include <stdio.h>
+
+#endif /* asTopology_h */
